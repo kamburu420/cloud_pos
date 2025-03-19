72e87670ed3e98ed43a25e6063fd022b7284b500
kamburu420 <kamburu36@gmail.com>
2025-03-20 03:19:26 +0500

    Add files via upload

--- app/forms/forms.py
+++ app/forms/forms.py
@@ -1,1 +1,23 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, FloatField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, EqualTo, DataRequired
 
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=150)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=6, max=150)])
+    submit = SubmitField('Login')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=150)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=6, max=150)])
+    confirm_password = PasswordField('Confirm Password', validators=[InputRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+
+class ProductForm(FlaskForm):
+    name = StringField('Product Name', validators=[DataRequired()])
+    description = StringField('Description')
+    price = FloatField('Price', validators=[DataRequired()])
+    stock_quantity = IntegerField('Stock Quantity', validators=[DataRequired()])
+    submit = SubmitField('Add Product')
